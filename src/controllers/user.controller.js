7dbf885a6a0485fa3ef461345b9800c556d8413f
rajtirole <rtech23456@gmail.com>
2024-04-30 10:14:06 +0530

    jfas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import User from "../models/user.model.js";
 import cloudinaryImageUploader from "../utils/cloudinary.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
 import fs from "fs";
 const generateAccessAndRefreshToken = async (userId) => {
